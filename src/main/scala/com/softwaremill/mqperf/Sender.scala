203f46ae96444107c2f50fee75f1afa852181832
adamw <adam@warski.org>
2014-06-03 15:55:21 +0200

    Testing the config reader/writer

--- src/main/scala/com/softwaremill/mqperf/Sender.scala
+++ src/main/scala/com/softwaremill/mqperf/Sender.scala
@@ -3,4 +3,9 @@ package com.softwaremill.mqperf
 object Sender extends App {
   println("Hello world! Args: " + args.toList)
   println("& bye!")
+
+  new S3TestConfig().whenChanged { content =>
+    println("Changed!!!")
+    println(content)
+  }
 }
