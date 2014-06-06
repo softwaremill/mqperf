42987730281509d6facac4f67256419c9d8cd511
adamw <adam@warski.org>
2014-06-06 09:36:53 +0200

    Stats

--- src/main/scala/com/softwaremill/mqperf/stats/ShowStats.scala
+++ src/main/scala/com/softwaremill/mqperf/stats/ShowStats.scala
@@ -5,7 +5,11 @@ import com.amazonaws.services.dynamodbv2.model.{AttributeValue, ComparisonOperat
 import scala.collection.JavaConversions._
 
 object ShowStats extends App with DynamoResultsTable {
-  private def fetchItemsWithPrefix(prefix: String) {
+  case class Result(start: String, end: String, took: Long, msgsCount: Int, _type: String) {
+    val msgsPerSecond = msgsCount.toDouble / took * 1000
+  }
+
+  private def fetchResultsWithPrefix(prefix: String): List[Result] = {
     val items = dynamoClient
       .scan(
         new ScanRequest(tableName).addScanFilterEntry(resultNameColumn,
@@ -14,8 +18,32 @@ object ShowStats extends App with DynamoResultsTable {
             .withAttributeValueList(new AttributeValue(prefix))))
       .getItems
 
-    items.foreach(println)
+    items
+      .map(i => Result(
+        i.get(startColumn).getS,
+        i.get(endColumn).getS,
+        i.get(tookColumn).getN.toLong,
+        i.get(msgsCountColumn).getN.toInt,
+        i.get(typeColumn).getS))
+      .toList
   }
 
-  fetchItemsWithPrefix("1401965734540-sqs1-s") //1401965734540-sqs1-s-372
+  val prefix = if (args.size > 0) args(0) else "sqs1-1401970126140"
+
+  val allResults = fetchResultsWithPrefix(prefix).sortBy(- _.msgsPerSecond)
+  val (sendResults, receiveResults) = allResults.partition(_._type == typeSend)
+  
+  def printResults(results: List[Result], _type: String) {
+    println(s"Results for $prefix, ${_type}")
+    results.foreach { r =>
+      println("%05.2f m/s: %dms, %d messages (%s -> %s)".format(r.msgsPerSecond, r.took, r.msgsCount, r.start, r.end))
+    }
+    val avg = results.map(_.msgsCount).sum.toDouble / results.map(_.took).sum * 1000
+    println("Average: %05.2f msgs/second".format(avg))
+
+    println("---\n")
+  }
+
+  printResults(sendResults, "send")
+  printResults(receiveResults, "receive")
 }
