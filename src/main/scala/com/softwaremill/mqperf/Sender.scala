e07aa841ae9b125c474c1a884fb6b63c4482de9f
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2016-12-23 12:29:13 +0100

    Gather metrics in sender/receiver

--- src/main/scala/com/softwaremill/mqperf/Sender.scala
+++ src/main/scala/com/softwaremill/mqperf/Sender.scala
@@ -1,6 +1,10 @@
 package com.softwaremill.mqperf
 
+import java.util.concurrent.TimeUnit
+
+import com.codahale.metrics.MetricRegistry
 import com.softwaremill.mqperf.mq.Mq
+
 import scala.util.Random
 import com.softwaremill.mqperf.config.TestConfigOnS3
 
@@ -32,20 +36,32 @@ object Sender extends App {
 class SenderRunnable(mq: Mq, reportResults: ReportResults,
     msg: String, msgCount: Int, maxSendMsgBatchSize: Int) extends Runnable {
 
+  val metricRegistry = new MetricRegistry()
+  val msgTimer = metricRegistry.timer("kafka-sender-messages-timer")
+  val histogram = metricRegistry.histogram("kafka-sender-messages-histogram")
+
   override def run() = {
     val mqSender = mq.createSender()
-    val start = System.currentTimeMillis()
-    doSend(mqSender)
-    val end = System.currentTimeMillis()
-    reportResults.reportSendingComplete(start, end, msgCount)
-    mqSender.close()
+    try {
+      doSend(mqSender)
+      TestMetrics.send(metricRegistry).foreach(reportResults.report)
+    }
+    finally {
+      mqSender.close()
+    }
   }
 
   private def doSend(mqSender: mq.MqSender) {
     var leftToSend = msgCount
     while (leftToSend > 0) {
       val batchSize = math.min(leftToSend, Random.nextInt(maxSendMsgBatchSize) + 1)
-      mqSender.send(List.fill(batchSize)(msg))
+      val batch = List.fill(batchSize)(msg)
+      val before = System.nanoTime()
+      mqSender.send(batch)
+      val after = System.nanoTime()
+      val nowSeconds = after / 1000000000L
+      batch.foreach(_ => histogram.update(nowSeconds))
+      msgTimer.update(after - before, TimeUnit.NANOSECONDS)
       leftToSend -= batchSize
     }
   }
