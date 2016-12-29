cb7867eeba07aa41fa914a2a1f62a1e8c8754e68
Marcin Kubala <marcin@mkubala.pl>
2016-12-29 12:35:56 +0100

    Set fixed default locale in ResultTest

--- src/test/scala/com/softwaremill/mqperf/stats/ResultTest.scala
+++ src/test/scala/com/softwaremill/mqperf/stats/ResultTest.scala
@@ -1,5 +1,7 @@
 package com.softwaremill.mqperf.stats
 
+import java.util.Locale
+
 import org.scalatest.{FlatSpec, Matchers}
 
 class ResultTest extends FlatSpec with Matchers {
@@ -7,12 +9,19 @@ class ResultTest extends FlatSpec with Matchers {
   behavior of "Result"
 
   it should "be correctly represented as string" in {
-    Result(
+    val defaultLocale = Locale.getDefault
+    Locale.setDefault(Locale.US)
+
+    val resultAsPrettyString = Result(
       3250346112L, "r", 21217, 21223, 21219.601627457203, 21221, 2.6071720196218555, 21223, 21224,
       21224.7, 21224, 7211L, 23301401L, 10627650.458852611, 9463284, 8410536.830270134, 23301401,
       23301301, 23201401, 23101401
-    ).toString should be(
-        """  3250346112 total messages
+    ).toString
+
+    Locale.setDefault(defaultLocale)
+
+    resultAsPrettyString should be(
+      """  3250346112 total messages
         |       21217 min msgs/s
         |       21223 max msgs/s
         |    21219.60 mean msgs/s
@@ -31,6 +40,6 @@ class ResultTest extends FlatSpec with Matchers {
         |       23.30 latency ms (95th percentile)
         |       23.20 latency ms (98th percentile)
         |       23.10 latency ms (99th percentile)""".stripMargin
-      )
+    )
   }
 }
