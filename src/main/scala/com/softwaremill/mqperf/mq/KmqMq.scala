59bf38500579b12f282b6aa589a09bfba6fffc1e
adamw <adam@warski.org>
2017-06-20 10:52:42 +0200

    Update timeout for tests

--- src/main/scala/com/softwaremill/mqperf/mq/KmqMq.scala
+++ src/main/scala/com/softwaremill/mqperf/mq/KmqMq.scala
@@ -49,7 +49,7 @@ class KmqMq(testConfig: TestConfig) extends Mq with StrictLogging {
 
   override def createReceiver() = {
     val kafkaClients = new KafkaClients(kafkaHosts)
-    val kmqConfig = new KmqConfig(Topic, MarkerTopic, GroupId, RedeliveryGroupId, 30.seconds.toMillis, 10000)
+    val kmqConfig = new KmqConfig(Topic, MarkerTopic, GroupId, RedeliveryGroupId, 15.seconds.toMillis, 10000)
 
     if (!receiverCreated.getAndSet(true)) {
       // this is the first receiver that is created
