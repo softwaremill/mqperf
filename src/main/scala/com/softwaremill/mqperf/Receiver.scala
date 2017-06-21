70909b2efdfad90bdffd7cc84a7acc3295d4294d
adamw <adam@warski.org>
2017-06-21 10:38:44 +0200

    More buckets

--- src/main/scala/com/softwaremill/mqperf/Receiver.scala
+++ src/main/scala/com/softwaremill/mqperf/Receiver.scala
@@ -26,7 +26,8 @@ object Receiver {
         .buckets(0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900,
           1000, 1250, 1500, 1750,
           2000, 2500, 3000, 3500, 4000, 4500,
-          5000, 6000, 7000, 8000, 9000, 10000)
+          5000, 6000, 7000, 8000, 9000, 10000,
+          20000, 30000, 40000, 50000, 60000)
         .labelNames(DefaultLabelNames: _*).register()
       val g = Gauge.build("mqperf_receive_threads_done", "number of receive threads done").labelNames(DefaultLabelNames: _*).register()
       g.labels(labelValues: _*).set(0)
