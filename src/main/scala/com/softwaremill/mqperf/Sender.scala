9b783cd3e1f935eb252178060471f01fd4540b1f
adamw <adam@warski.org>
2017-06-28 15:13:20 +0200

    Collect send latencies

--- src/main/scala/com/softwaremill/mqperf/Sender.scala
+++ src/main/scala/com/softwaremill/mqperf/Sender.scala
@@ -2,7 +2,7 @@ package com.softwaremill.mqperf
 
 import com.softwaremill.mqperf.config.TestConfig
 import com.softwaremill.mqperf.mq.Mq
-import com.softwaremill.mqperf.util.PrometheusMetricServer
+import com.softwaremill.mqperf.util.{Clock, PrometheusMetricServer, RealClock}
 import com.typesafe.scalalogging.StrictLogging
 import io.prometheus.client.{CollectorRegistry, Counter, Gauge, Histogram}
 
@@ -20,11 +20,17 @@ object Sender {
       val c = Counter.build("mqperf_sent_total", "number of sent messages").labelNames(DefaultLabelNames: _*).register()
       val g = Gauge.build("mqperf_sent_threads_done", "number of sent threads done").labelNames(DefaultLabelNames: _*).register()
       g.labels(labelValues: _*).set(0)
+      val h = Histogram.build("mqperf_send_latency_ms", "latency of sent messages")
+        .buckets(0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900,
+          1000, 1250, 1500, 1750,
+          2000, 2500, 3000, 3500, 4000, 4500, 5000)
+        .labelNames(DefaultLabelNames: _*).register()
       val sr = new SenderRunnable(
         mq,
         testConfig.mqType, Msg.prefix(testConfig),
         testConfig.msgCountPerThread, testConfig.maxSendMsgBatchSize,
-        c.labels(labelValues: _*), g.labels(labelValues: _*)
+        c.labels(labelValues: _*), g.labels(labelValues: _*),
+        h.labels(labelValues: _*)
       )
 
       val threads = (1 to testConfig.senderThreads).map { _ =>
@@ -42,7 +48,8 @@ object Sender {
 
 class SenderRunnable(mq: Mq, mqType: String,
   msgPrefix: String, msgCount: Int, maxSendMsgBatchSize: Int,
-  sendCounter: Counter.Child, sendDone: Gauge.Child)
+  sendCounter: Counter.Child, sendDone: Gauge.Child, sendLatency: Histogram.Child,
+  clock: Clock = RealClock)
     extends Runnable with StrictLogging {
 
   override def run() = {
@@ -55,7 +62,9 @@ class SenderRunnable(mq: Mq, mqType: String,
         val fullMsg = Msg.addTimestamp(msgPrefix)
         val batch = List.fill(batchSize)(fullMsg)
         logger.debug("Sending batch")
+        val start = clock.currentTimeMillis()
         mqSender.send(batch)
+        sendLatency.observe(clock.currentTimeMillis() - start)
         leftToSend -= batchSize
         sendCounter.inc(batchSize)
       }
