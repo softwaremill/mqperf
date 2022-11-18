45a403ec12d2d437f8d7258dd160a9fc0d937657
Wiktor Wojda <wiktor.wojda@gmail.com>
2022-11-18 16:21:19 +0100

    Feature/client app to deployment (#96)
    * Client app from pod -> deployment
    
    * terraform fmt

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,5 +1,12 @@
-
 resource "kubectl_manifest" "app" {
   yaml_body = templatefile("app-manifests/app.yaml", {
-    "APP_IMAGE" = var.app_image})
+  "APP_IMAGE" = var.app_image, "APP_MAX_NODES_NUMBER" = var.app_max_nodes_number })
+}
+
+resource "kubectl_manifest" "service" {
+  yaml_body = file("app-manifests/service.yaml")
+}
+
+resource "kubectl_manifest" "servicemonitor" {
+  yaml_body = file("app-manifests/servicemonitor.yaml")
 }
--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -2,3 +2,9 @@ variable "app_image" {
   type        = string
   description = "Client app image from config file"
 }
+
+variable "app_max_nodes_number" {
+  type        = number
+  description = "Number of nodes client app should be deployed on"
+  default     = 1
+}
