3f1ccfc03e078760dc45302e01a40f7843802aaa
Michał Ossowski <michal.ossowski@softwaremill.com>
2022-11-29 17:13:50 +0100

    add script for running tests on cluster

---
+++ terraform/run_test.py
@@ -0,0 +1,78 @@
+import requests
+import json
+import time
+import sys
+from kubernetes import client, config
+from kubernetes.stream import portforward
+import portforward
+import _portforward
+from pathlib import Path
+from datetime import datetime
+
+
+def run(test_file: str):
+    with open(test_file) as f:
+        payload = json.load(f)
+
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    namespace = 'default'
+    app_pods = [pod for pod in v1.list_namespaced_pod(namespace, watch=False).items if pod.metadata.name.startswith('app-deployment')]
+
+    print("MQPerf app pods:")
+    for pod in app_pods:
+        print(pod.metadata.name)
+
+    instances = []
+    port = 8080
+    config_path = str(Path.home() / ".kube" / "config")
+    for idx, pod in enumerate(app_pods):
+        local_port = port + idx
+        _portforward.forward(namespace, pod.metadata.name, local_port, port, config_path, portforward.LogLevel.DEBUG.value)
+        instances.append((pod, local_port, f"http://localhost:{local_port}"))
+    time.sleep(5) # sleep to ensure port forwarding is operational
+
+    _, _, base_url = instances[0]
+
+    requests.post(base_url + '/cleanup', json=payload)
+    print('Cleanup complete')
+
+    check_if_ok(requests.post(base_url + '/init', json=payload))
+    print('Initialized')
+
+    for (pod_name, _, url), job in zip(instances, payload['jobs']):
+        check_if_ok(requests.post(url + '/start/' + job, json=payload))
+        print(f'Job {job} started on {pod_name}')
+
+    start = datetime.utcnow()
+
+    while True:
+        jobs_in_progress = []
+        for _, _, url in instances:
+            jobs_in_progress.extend(requests.get(url + '/in-progress').json())
+        if not jobs_in_progress:
+            break
+        else:
+            print('Still running:', jobs_in_progress)
+            time.sleep(10)
+
+    end = datetime.utcnow()
+
+    print('Test time range:')
+    print(start.isoformat())
+    print(end.isoformat())
+
+    requests.post(base_url + '/cleanup', json=payload)
+    print('Cleanup complete')
+
+    # TODO: acquire metrics, save dashboard, etc.
+
+
+def check_if_ok(resp):
+    if not resp.ok:
+        raise Exception('Request failed: ' + resp)
+    return resp
+
+
+if __name__ == '__main__':
+    run(sys.argv[1])
