b406de41b5aea71b23defa3112b9b7e42b917007
Pawel Stepniewski <ps.stepniewski@gmail.com>
2022-12-27 15:19:53 +0100

    [PG][doobie] Implements sender/start and receiver/start

--- clients/postgres/src/main/scala/mqperf/postgres/PostgresMq.scala
+++ clients/postgres/src/main/scala/mqperf/postgres/PostgresMq.scala
@@ -2,20 +2,27 @@ package mqperf.postgres
 
 import cats.effect._
 import cats.effect.unsafe.IORuntime
+import cats.implicits.catsSyntaxApplicativeId
+import cats.syntax.seq._
 import com.typesafe.scalalogging.StrictLogging
 import com.zaxxer.hikari.HikariConfig
-import doobie._
 import doobie.hikari.HikariTransactor
 import doobie.implicits._
+import doobie.postgres.implicits._
+import doobie.util.fragment.Fragment.const
+import doobie.util.fragments.in
+import doobie._
 import mqperf.{Config, Mq, MqReceiver, MqSender}
 
+import java.time.OffsetDateTime
 import java.util.UUID
-import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future}
 
 class PostgresMq(clock: java.time.Clock) extends Mq with StrictLogging {
 
   implicit val ioRuntime: IORuntime = cats.effect.unsafe.IORuntime.global
+  implicit val exContext: ExecutionContext = scala.concurrent.ExecutionContext.global
 
   private val HostConfigKey = "host"
   private val PortConfigKey = "port"
@@ -29,8 +36,16 @@ class PostgresMq(clock: java.time.Clock) extends Mq with StrictLogging {
     val tx = transactor(config)
 
     val initQueries: ConnectionIO[Int] = for {
-      _ <- (sql"create table if not exists " ++ Fragment.const(table) ++ sql"(id uuid primary key, content text not null, next_delivery timestamptz not null)").update.run
-      v <- (sql"create index if not exists next_delivery_idx on " ++ Fragment.const(table) ++ sql"(next_delivery)").update.run
+      _ <- (
+        fr"create table if not exists"
+          ++ const(table)
+          ++ fr"(id uuid primary key, content text not null, next_delivery timestamptz not null)"
+      ).update.run
+      v <- (
+        fr"create index if not exists next_delivery_idx on "
+          ++ const(table)
+          ++ fr"(next_delivery)"
+      ).update.run
     } yield v
 
     initQueries
@@ -40,12 +55,11 @@ class PostgresMq(clock: java.time.Clock) extends Mq with StrictLogging {
     logger.info(s"Table $table created.")
   }
 
-
   override def cleanUp(config: Config): Unit = {
     val table = config.mqConfig(TableConfigKey)
     val tx = transactor(config)
 
-    (sql"drop table if exists " ++ Fragment.const(table)).update.run
+    (sql"drop table if exists " ++ const(table)).update.run
       .transact(tx)
       .unsafeRunSync()
 
@@ -54,101 +68,82 @@ class PostgresMq(clock: java.time.Clock) extends Mq with StrictLogging {
 
   override def createSender(config: Config): MqSender = new MqSender {
 
-//    private val senderPool = connectionPool(config, config.senderConcurrency)
-//    private val client: DatabaseClient = pooledDatabaseClient(senderPool)
     private val table = config.mqConfig(TableConfigKey)
+    private val (tx, closure) = pooledTransactor(config, config.senderConcurrency).allocated.unsafeRunSync()
 
     override def send(msgs: Seq[String]): Future[Unit] = {
 
-      Future.successful(())
-//      val query = new StringBuilder(s"insert into $table(id, content, next_delivery) values ")
-//      query ++= s"('${UUID.randomUUID()}', '${msgs.head}', '${ZonedDateTime.now(clock)}')"
-//      msgs.tail.foreach(m => query ++= s", ('${UUID.randomUUID()}', '$m', '${ZonedDateTime.now(clock)}')")
-//
-//      client
-//        .sql(query.toString)
-//        .fetch()
-//        .rowsUpdated()
-//        .toFuture
-//        .asScala
-//        .map(_ => ())
+      val insertRecords = msgs.map(m => (UUID.randomUUID(), m, OffsetDateTime.now()))
+      val insertQuery = s"insert into $table(id, content, next_delivery) values (?, ?, ?)"
+
+      Update[(UUID, String, OffsetDateTime)](insertQuery)
+        .updateMany(insertRecords)
+        .transact(tx)
+        .unsafeToFuture()
+        .map(_ => ())
     }
 
-    override def close(): Future[Unit] =
-      Future.successful(())
-//      senderPool
-//        .disposeLater()
-//        .toFuture
-//        .asScala
-//        .map(_ => ())
+    override def close(): Future[Unit] = {
+      logger.info("Closing sender connection pool")
+      closure.unsafeToFuture()
+    }
   }
 
   override def createReceiver(config: Config): MqReceiver = new MqReceiver {
 
     override type MsgId = UUID
 
-//    private val receiverPool: ConnectionPool = connectionPool(config, config.receiverConcurrency)
-//    private val client: DatabaseClient = pooledDatabaseClient(receiverPool)
-//    private val txOperator: TransactionalOperator = TransactionalOperator.create(new R2dbcTransactionManager(receiverPool))
-//    private val table = config.mqConfig(TableConfigKey)
-//
-//    private val selectQuery = client
-//      .sql(s"select id, content from $table where next_delivery <= :now limit :limit for update skip locked")
-//    private val updateQuery = client
-//      .sql(s"update $table set next_delivery = :nextDelivery where id in (:inIds)")
-//    private val deleteQuery = client
-//      .sql(s"delete from $table where id in (:inIds)")
+    private val table = config.mqConfig(TableConfigKey)
+    private val (tx, closure) = pooledTransactor(config, config.receiverConcurrency).allocated.unsafeRunSync()
 
     override def receive(maxMsgCount: Int): Future[Seq[(MsgId, String)]] = {
-      Future.successful(Seq())
-//      val now = ZonedDateTime.now(clock)
-//      val nextDelivery = now.plusSeconds(100)
-//
-//      selectQuery
-//        .bind("now", now)
-//        .bind("limit", maxMsgCount)
-//        .map(row => (row.get("id", classOf[UUID]), row.get("content", classOf[String])))
-//        .all()
-//        .collectList()
-//        .map(_.asScala.toList)
-//        .flatMap {
-//          case Nil =>
-//            Mono.just(Seq.empty[(MsgId, String)])
-//          case ids =>
-//            updateQuery
-//              .bind("nextDelivery", nextDelivery)
-//              .bind("inIds", ids.map(_._1).asJava)
-//              .fetch()
-//              .rowsUpdated()
-//              .map(_ => ids)
-//        }
-//        .as((v: Mono[Seq[(MsgId, String)]]) => txOperator.transactional(v))
-//        .toFuture
-//        .asScala
+      val now = OffsetDateTime.now(clock)
+      val nextDelivery = now.plusSeconds(100)
+      val receiveQueries = for {
+        msgs <- (
+          fr"select id, content from "
+            ++ const(table)
+            ++ fr"where next_delivery <= $now"
+            ++ fr"limit $maxMsgCount"
+            ++ fr"for update skip locked"
+        ).query[(MsgId, String)].to[Seq]
+        _ <- msgs.toNeSeq
+          .map(nes =>
+            (
+              fr"update"
+                ++ const(table)
+                ++ fr"set next_delivery = $nextDelivery"
+                ++ fr"where"
+                ++ in(fr"id", nes.map(_._1))
+            ).update.run
+          )
+          .getOrElse(0.pure[ConnectionIO])
+      } yield msgs
+
+      receiveQueries
+        .transact(tx)
+        .unsafeToFuture()
     }
 
-    override def ack(ids: Seq[MsgId]): Future[Unit] =
-      Future.successful(())
-//      ids match {
-//      case Nil =>
-//        Future.successful(())
-//      case vs =>
-//        deleteQuery
-//          .bind("inIds", vs.asJava)
-//          .fetch()
-//          .rowsUpdated()
-//          .toFuture
-//          .asScala
-//          .map(_ => ())
-//    }
+    override def ack(ids: Seq[MsgId]): Future[Unit] = {
+      ids.toNeSeq
+        .map(nes =>
+          (
+            fr"delete from"
+              ++ const(table)
+              ++ fr"where"
+              ++ in(fr"id", nes)
+          ).update.run
+            .transact(tx)
+            .unsafeToFuture()
+            .map(_ => ())
+        )
+        .getOrElse(Future.successful(()))
+    }
 
     override def close(): Future[Unit] = {
-      Future.successful(())
-//      receiverPool
-//        .disposeLater()
-//        .toFuture
-//        .asScala
-//        .map(_ => ())
+      logger.info("Closing receiver connection pool")
+      closure.unsafeToFuture()
     }
   }
 
@@ -165,7 +160,6 @@ class PostgresMq(clock: java.time.Clock) extends Mq with StrictLogging {
       tx <- HikariTransactor.fromHikariConfig[IO](
         {
           val hc = new HikariConfig()
-          hc.setDataSourceClassName("org.postgresql.Driver")
           hc.setUsername(config.mqConfig(UsernameConfigKey))
           hc.setPassword(config.mqConfig(PasswordConfigKey))
           hc.setJdbcUrl(jdbcUrl(config))
