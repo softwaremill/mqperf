dba6cf2c49d60c55d7da367be9410109d66cb3b3
Pawel Stepniewski <ps.stepniewski@gmail.com>
2022-12-29 10:40:40 +0100

    Moves sender to IO & cats

--- build.sbt
+++ build.sbt
@@ -36,6 +36,8 @@ lazy val core: Project = (project in file("clients/core"))
       "com.softwaremill.sttp.tapir" %% "tapir-netty-server" % tapirVersion,
       "com.softwaremill.sttp.tapir" %% "tapir-prometheus-metrics" % tapirVersion,
       "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % tapirVersion,
+      "org.typelevel" %% "cats-effect" % "3.5-6581dc4",
+      "org.typelevel" %% "cats-core" % "2.9.0",
       scalaTest
     )
   )
--- clients/core/src/main/scala/mqperf/Sender.scala
+++ clients/core/src/main/scala/mqperf/Sender.scala
@@ -1,20 +1,19 @@
 package mqperf
 
+import cats.effect._
+import cats.effect.unsafe.IORuntime
+import cats.implicits._
 import com.typesafe.scalalogging.StrictLogging
 import io.prometheus.client.{Counter, Histogram}
 
 import java.time.Clock
-import java.util.concurrent.Executors
-import java.util.concurrent.atomic.AtomicInteger
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
-import scala.concurrent.{ExecutionContext, Future, blocking}
-import scala.util.Failure
 
 class Sender(config: Config, mq: Mq, clock: Clock) extends StrictLogging {
 
+  implicit val ioRuntime: IORuntime = cats.effect.unsafe.IORuntime.global
   private val messagesPool = RandomMessagesPool(config.msgSizeBytes)
-  private val loopEc = ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor())
 
   private object LocalMetrics {
     private val testIdLabelValue = config.testId
@@ -22,61 +21,106 @@ class Sender(config: Config, mq: Mq, clock: Clock) extends StrictLogging {
     val messageLatencyHistogram: Histogram.Child = Metrics.Sender.messageLatencyHistogram.labels(testIdLabelValue)
   }
 
+  /*
+    sent - number of messages already sent
+    permits - number of messages which sender is allowed to send
+    currentIteration - number of iteration, 1 iteration lasts 1 second
+    nextIterationHook - allows sender to wait for next iteration without thread-blocking. If Deferred will evaluate to
+      false sender will start nextIteration, otherwise it will finish sending.
+   */
+  case class State(sent: Long, permits: Long, currentIteration: Int, nextIterationHook: Deferred[IO, Boolean])
+
   def run(): Future[Unit] = {
     val mqSender = mq.createSender(config)
-    val end = clock.millis() + config.testLengthSeconds.seconds.toMillis
-    runIterations(mqSender, end)
-      .map { _ =>
-        logger.info("Sending done, waiting for all messages to be flushed ...")
-        Thread.sleep(3.seconds.toMillis)
-      }
-      .flatMap(_ => mqSender.close())
+
+    val senderRun = runParallel(mqSender, config.senderConcurrency) >>
+      IO(logger.info("Sending done, waiting for all messages to be flushed ...")) >>
+      IO.sleep(3.seconds) >>
+      IO.fromFuture(IO(mqSender.close()))
+
+    senderRun
+      .handleError(e => logger.error("Sender failed", e))
+      .unsafeToFuture()
   }
 
-  private def runIterations(mqSender: MqSender, testEnd: Long): Future[Unit] = {
+  private def runParallel(mqSender: MqSender, senderConcurrency: Int): IO[Unit] = {
     val batchSize = config.batchSizeSend
-    val sentMessages = new AtomicInteger(0)
-    val permits = new AtomicInteger(0)
 
-    def send(): Future[Unit] = {
-      if (clock.millis() >= testEnd) {
-        Future.successful(())
-      } else if (sentMessages.get() < permits.get()) {
-        val sendStart = clock.millis()
-        sentMessages.addAndGet(batchSize)
-        mqSender
-          .send(nextMessagesBatch(batchSize))
-          .map { _ =>
-            LocalMetrics.messageCounter.inc(batchSize.toDouble)
-            LocalMetrics.messageLatencyHistogram.observe((clock.millis() - sendStart).toDouble)
+    def sendRunner(state: Ref[IO, State], iterationNo: Int = 0): IO[Unit] = {
+      def iteration(state: Ref[IO, State]): IO[Unit] = {
+        state.get.flatMap(s =>
+          if (s.sent >= s.permits) {
+            IO.unit
           }
-          .flatMap(_ => send())
-      } else {
-        // do nothing, just wait for either: increasing of the permits or the test end
-        send()
+          else {
+            for {
+              sendStart <- IO(clock.millis())
+              _ <- IO.fromFuture(IO(mqSender.send(nextMessagesBatch(batchSize))))
+              _ <- state.update { s => s.copy(sent = s.sent + batchSize) }
+              _ <- IO {
+                LocalMetrics.messageCounter.inc(batchSize.toDouble)
+                LocalMetrics.messageLatencyHistogram.observe((clock.millis() - sendStart).toDouble)
+              }
+              result <- iteration(state)
+            } yield result
+          }
+        )
+        .handleErrorWith{ e =>
+          IO(logger.error(s"Sender iteration[$iterationNo] failed ", e)) >>
+            iteration(state)
+        }
+      }
+
+      state.get.flatMap(s => {
+        if(iterationNo < s.currentIteration) {
+          iteration(state) >> sendRunner(state, iterationNo + 1)
+        }
+        else {
+          s.nextIterationHook.get.flatMap(isTestEnd => {
+            if (isTestEnd) IO.unit
+            else iteration(state) >> sendRunner(state, iterationNo + 1)
+          })
+        }
+      })
+    }
+
+    def loop(until: Long, state: Ref[IO, State], iterationNo: Int = 0): IO[Unit] = {
+      if (clock.millis() >= until) {
+        state
+          .get
+          .flatMap(_.nextIterationHook.complete(true))
+          .void
+      }
+      else {
+        for {
+          nextIterationH <- Deferred[IO, Boolean]
+          _              <- state.get.flatMap(_.nextIterationHook.complete(false))
+          addedPermits   <- state.modify(s => {
+                              val p = newPermits(s.sent, s.permits)
+                              s.copy(
+                                permits = s.permits + p,
+                                currentIteration = s.currentIteration + 1,
+                                nextIterationHook = nextIterationH
+                              ) -> p
+                            })
+          _              <- IO(logger.info(s"Messages to send: $addedPermits (concurrency=${config.senderConcurrency})"))
+          _              <- IO.sleep(1.second)
+          result         <- loop(until, state, iterationNo + 1)
+        } yield result
       }
     }
 
-    // increases permits number every second
-    Future {
-      while (clock.millis() < testEnd) {
-        val startTimestamp = clock.millis()
-        permits.addAndGet(config.msgsPerSecond)
-        logger.info(s"Messages to send: ${config.msgsPerSecond} (concurrency=${config.senderConcurrency})")
-        val endTimestamp = clock.millis()
-        Thread.sleep(Math.max(0, 1.second.toMillis - (endTimestamp - startTimestamp)))
-      }
-    }(loopEc)
+    def newPermits(sent: Long, permits: Long) = {
+      if (permits - sent > config.msgsPerSecond) 0
+      else config.msgsPerSecond
+    }
 
-    val senderConcurrency = config.senderConcurrency
-    Future
-      .sequence(
-        (1 to senderConcurrency).map { _ => send() }
-      )
-      .andThen { case Failure(ex) =>
-        logger.error("Sending iteration failure", ex)
-      }
-      .map(_ => ())
+    for {
+      initHook  <- Deferred[IO, Boolean]
+      initState <- Ref.of[IO, State](State(0, 0, 0, initHook))
+      testEnd   <- IO(clock.millis() + config.testLengthSeconds.seconds.toMillis)
+      runners   <- (loop(testEnd, initState) :: (1 to senderConcurrency).map(_ => sendRunner(initState)).toList).parSequence.void
+    } yield runners
   }
 
   private def nextMessagesBatch(batchSize: Int): Seq[String] = {
